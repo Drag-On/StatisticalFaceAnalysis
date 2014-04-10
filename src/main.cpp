7e08ce7cec2da1edcf7724a16326b364d5f9f3e6
Jan Möller <jan.moeller@hs-furtwangen.de>
2014-04-10 16:07:37 +0200

    Autofocus for camera in origin

--- src/main.cpp
+++ src/main.cpp
@@ -20,6 +20,8 @@
 #include <Rendering/ShaderProgram.h>
 #include <Rendering/Camera.h>
 #include <Math/Utility.h>
+#include <Math/Matrix3x3.h>
+#include <Math/Matrix4x4.h>
 #include <Math/Quaternion.h>
 #include "SFA/Model.h"
 #include "NearestNeighbor/SimpleNearestNeighbor.h"
@@ -35,8 +37,9 @@ Model* pSourceModel, *pDestModel;
 ShaderProgram* pShader;
 Vec3f colorSrc(1.0f, 0.0f, 0.0f), colorDest(0.0f, 1.0f, 0.0f);
 Camera* pCam;
+float camDist = 3;
 Mat4f view, projection;
-float mouseSpeed = 1.5, moveSpeed = 2.5;
+float moveSpeed = 2.5;
 
 bool showSource = true, showDest = true;
 
@@ -45,19 +48,32 @@ RigidPointICP icp;
 
 Properties properties;
 
+void updateViewProjection()
+{
+    view = Mat4f::makeView(pCam->position(), pCam->rotation() * Vec3f(0, 0, 1),
+	    pCam->rotation() * Vec3f(0, 1, 0));
+    projection = Mat4f::makeProjection(pCam->getFieldOfView(),
+	    float(pWnd->getFrameWidth()) / pWnd->getFrameHeight(), pCam->getNear(), pCam->getFar());
+}
+
+void moveCamera(double x, double y)
+{
+    pCam->rotate(x, y);
+    Vec3f dir;
+    pCam->getOrientation(&dir, nullptr, nullptr);
+    pCam->position() = -dir * camDist;
+}
+
 void scrollCallback(Window::ScrollEventArgs const& args)
 {
     // Zoom
     pCam->setFieldOfView(pCam->getFieldOfView() + 0.1f * args.yOffset);
-    projection = Mat4f::makeProjection(pCam->getFieldOfView(),
-	    float(pWnd->getFrameWidth()) / pWnd->getFrameHeight(),
-	    pCam->getNear(), pCam->getFar());
+    updateViewProjection();
 }
 
-void framebufferResizeCallback(Window::FramebufferResizeEventArgs const& args)
+void framebufferResizeCallback(Window::FramebufferResizeEventArgs const& /*args*/)
 {
-    projection = Mat4f::makeProjection(pCam->getFieldOfView(),
-	    float(args.width) / args.height, pCam->getNear(), pCam->getFar());
+    updateViewProjection();
 }
 
 void keyCallback(Window::KeyEventArgs const& args)
@@ -102,37 +118,24 @@ void updateCallback(Window::UpdateEventArgs const& args)
 {
     auto deltaTime = args.deltaTime;
 
-    // Update mouse
-    double x, y;
-    pWnd->getCursorPos(x, y);
-    float horizontal = deltaTime * mouseSpeed
-	    * float(pWnd->getFrameWidth() / 2 - x);
-    float vertical = deltaTime * mouseSpeed
-	    * float(pWnd->getFrameHeight() / 2 - y);
-    pCam->rotate(horizontal, -vertical);
-    // Camera vectors
-    Vec3f direction = pCam->rotation() * Vec3f(0, 0, 1);
-    Vec3f right = pCam->rotation() * Vec3f(-1, 0, 0);
-    // Reset mouse position to center of the screen
-    pWnd->setCursorPos(pWnd->getFrameWidth() / 2, pWnd->getFrameHeight() / 2);
+    // Update camera position and rotation
+    double x = 0, y = 0;
+    if (pWnd->getKey(GLFW_KEY_W) == GLFW_PRESS)
+	y += deltaTime * moveSpeed;
+    if (pWnd->getKey(GLFW_KEY_S) == GLFW_PRESS)
+	y -= deltaTime * moveSpeed;
+    if (pWnd->getKey(GLFW_KEY_A) == GLFW_PRESS)
+	x -= deltaTime * moveSpeed;
+    if (pWnd->getKey(GLFW_KEY_D) == GLFW_PRESS)
+	x += deltaTime * moveSpeed;
+    if (pWnd->getKey(GLFW_KEY_E) == GLFW_PRESS)
+    	camDist -= deltaTime * moveSpeed;
+    if (pWnd->getKey(GLFW_KEY_Q) == GLFW_PRESS)
+	camDist += deltaTime * moveSpeed;
+    moveCamera(x, y);
 
     // Update view matrix
-    view = Mat4f::makeView(pCam->position(), pCam->rotation() * Vec3f(0, 0, 1),
-	    pCam->rotation() * Vec3f(0, 1, 0));
-
-    // Update keyboard
-    if (pWnd->getKey(GLFW_KEY_W) == GLFW_PRESS)
-	pCam->position() += direction * deltaTime * moveSpeed;
-    if (pWnd->getKey(GLFW_KEY_A) == GLFW_PRESS)
-	pCam->position() -= right * deltaTime * moveSpeed;
-    if (pWnd->getKey(GLFW_KEY_S) == GLFW_PRESS)
-	pCam->position() -= direction * deltaTime * moveSpeed;
-    if (pWnd->getKey(GLFW_KEY_D) == GLFW_PRESS)
-	pCam->position() += right * deltaTime * moveSpeed;
-    if (pWnd->getKey(GLFW_KEY_E) == GLFW_PRESS)
-	pCam->position() += Vec3f(0, 1, 0) * deltaTime * moveSpeed;
-    if (pWnd->getKey(GLFW_KEY_Q) == GLFW_PRESS)
-	pCam->position() -= Vec3f(0, 1, 0) * deltaTime * moveSpeed;
+    updateViewProjection();
 }
 
 void renderCallback(Window::RenderEventArgs const& args)
@@ -144,8 +147,7 @@ void renderCallback(Window::RenderEventArgs const& args)
     GLint mvpId = pShader->getDefaultUniformHandle(ShaderProgram::Uniform::MVP);
     if (mvpId >= 0)
     {
-	pShader->setUniformFloatMatrix4Array(mvpId, 1, GL_FALSE,
-		mvp.getDataPointer());
+	pShader->setUniformFloatMatrix4Array(mvpId, 1, GL_FALSE, mvp.getDataPointer());
     }
     // ITMV matrix
     GLint itmvId = pShader->getDefaultUniformHandle(
@@ -153,8 +155,7 @@ void renderCallback(Window::RenderEventArgs const& args)
     if (itmvId >= 0)
     {
 	// In this case inverse transpose of view equals view
-	pShader->setUniformFloatMatrix4Array(itmvId, 1, GL_FALSE,
-		view.getDataPointer());
+	pShader->setUniformFloatMatrix4Array(itmvId, 1, GL_FALSE, view.getDataPointer());
     }
 
     // Draw
@@ -187,20 +188,16 @@ int main(int argc, char** argv)
     // Initialize it
     pWnd->init(true, false, false);
     // Add a camera
-    Vec3f direction = Vec3f(1, -2.5, -3);
-    pCam = new Camera(Vec3f(-1, 2, 3), direction, Vec3f(1, 0, 0).cross(direction), pi_4(), 0.1, 100);
-    // Calculate model and view matrix
-    view = Mat4f::makeView(pCam->position(),
-	    pCam->rotation() * Vec3f(0, 0, 1), pCam->rotation() * Vec3f(0, 1, 0));
-    projection = Mat4f::makeProjection(pCam->getFieldOfView(),
-	    float(pWnd->getFrameWidth()) / pWnd->getFrameHeight(), pCam->getNear(), pCam->getFar());
+    Vec3f pos = Vec3f(0, 0, camDist);
+    Vec3f dir = -pos;
+    pCam = new Camera(pos, dir, Vec3f(1, 0, 0).cross(dir), pi_4(), 0.1, 100);
     // Load mesh, shader and texture
     pSourceModel = new Model(properties.getStringValue("src"));
     pDestModel = new Model(properties.getStringValue("dest"));
     pSourceModel->getBasePointer()->updateBuffers();
     pDestModel->getBasePointer()->updateBuffers();
     pShader = ShaderProgram::createSimpleColorShader();
-    // Add render callback so we can draw the mesh
+    // Add callbacks
     pWnd->addUpdateCallback(std::bind(&updateCallback, std::placeholders::_1));
     pWnd->addRenderCallback(std::bind(&renderCallback, std::placeholders::_1));
     pWnd->addScrollCallback(std::bind(&scrollCallback, std::placeholders::_1));
