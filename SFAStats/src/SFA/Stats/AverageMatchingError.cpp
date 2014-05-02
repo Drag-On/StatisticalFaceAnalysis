b997b04e6558966f172b9d6b4aaae0931dac7680
Jan Möller <jan.moeller@hs-furtwangen.de>
2014-05-02 15:24:28 +0200

    Fixed selection filter being reset to "all points"

--- SFAStats/src/SFA/Stats/AverageMatchingError.cpp
+++ SFAStats/src/SFA/Stats/AverageMatchingError.cpp
@@ -100,6 +100,7 @@ namespace sfa
     {
 	// Store original vertex positions
 	Model original(src);
+	unsigned int selectionMethod = icp.getSelectionMethod();
 	icp.setSelectionMethod(ICP::NO_EDGES);
 	// Calculate a lot if icp steps to make sure we have the correct pairs
 	for (unsigned int i = 0; i < icpCycles; i++)
@@ -114,7 +115,7 @@ namespace sfa
 	    correctPairs[i] = nn.getNearest(i, src, dest);
 	}
 	// Revert back to original vertex positions
-	icp.setSelectionMethod(0);
+	icp.setSelectionMethod(selectionMethod);
 	src = std::move(original);
     }
 
